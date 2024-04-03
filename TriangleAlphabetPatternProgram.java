4d9df118149862aa50779a1bf6f8a80b47c8f122
Vansh Sethi <vanshsethi.usar@gmail.com>
2024-04-03 20:19:44 +0530

    added trianglealphabetpattern program

---
+++ TriangleAlphabetPatternProgram.java
@@ -0,0 +1,15 @@
+public class TriangleAlphabetPatternProgram {
+    public static void main(String[] args) {
+        int n = 9;
+        for(int i = 1; i <= n; i++) {
+            int p = 'A';
+            for(int j = i; j <= n; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++) {
+                System.out.print((char)p++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
