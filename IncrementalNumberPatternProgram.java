11391e23e67423682d7db83c4af79c967a8432b6
Vansh Sethi <vanshsethi.usar@gmail.com>
2024-04-03 19:46:36 +0530

    added incrementalnumberpattern program

---
+++ IncrementalNumberPatternProgram.java
@@ -0,0 +1,12 @@
+public class IncrementalNumberPatternProgram {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            int p = 1;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(p++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
